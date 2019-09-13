cfc94f577f458d7fef9182e65c4e55fe17823fd6
ocortezm1 <53576455+ocortezm1@users.noreply.github.com>
2019-09-13 11:56:59 -0400

    Create combo.php

---
+++ combo.php
@@ -0,0 +1,19 @@
+<?php
+    include "../db.php";
+    $con = connect();
+    $sql="SELECT codigoespecialidad,nombreespecialidad FROM ESPECIALIDAD";
+    $resultado = $con->query($sql);
+
+    $html= "<option value='0'>Especialidad</option>";
+
+	while($rowN = $resultado->fetch_assoc())
+	{
+		$html.= "<option value='".$rowN['codigoespecialidad']."'>".$rowN['nombreespecialidad']."</option>";
+	}
+?>
+
+<div>
+<select>
+<?php echo $html;?>
+</select>
+</div>
