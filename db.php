92a2c383c8b27b059f96fb914d28292325316083
ocortezm1 <53576455+ocortezm1@users.noreply.github.com>
2019-09-13 12:05:35 -0400

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 // simple conexion a la base de datos
 function connect(){
-	return new mysqli("localhost","user_root","Password","Name_BD");
+	return new mysqli("localhost","user_root","","MEDICOS");
 }
 ?>
